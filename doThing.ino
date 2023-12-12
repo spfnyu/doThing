2ed7918feb966183dda7c34421a2a5a960185e59
spfnyu <smp9330@nyu.edu>
2023-12-11 21:46:02 -0500

    Update doThing.ino

--- doThing.ino
+++ doThing.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+int ledPin = 13;
+int ledPin2 = 14;
+int ledPin3 = 15;
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+    pinMode(ledPin3, OUTPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  digitalWrite(ledPin3, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW);
+  digitalWrite(ledPin3, LOW);
+  delay(500);
 
 }
